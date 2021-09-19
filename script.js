95c7f0e365d9ed1caf64a6007e564e17a87327f7
Alex Groat <alex.groat.95@gmail.com>
2021-09-19 15:16:46 +0930

    Initial commit variables listed and new folder created for work

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword()
+
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = ["$", "%", "^", "&" , "@", "!", "/", "+", "-", "(", ")"];
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
