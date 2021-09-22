993f3a350e5f574cd74a71cd138ee31fafd8c761
Alex Groat <alex.groat.95@gmail.com>
2021-09-22 20:21:33 +0930

    Third Commit Restart with new way explained in slack channel by Ben Fawcett

---
+++ Assets/script.js
@@ -0,0 +1,29 @@
+function generatePassword () {
+
+  //Each character set is displayed in its own individual array.
+
+  var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "["];
+  var availableCharacters = [];
+
+  var passwordLength = getPasswordLength();
+
+  var 
+
+}
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
